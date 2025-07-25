68ec2e5eaf4ccd3cc464b6997b59d476e55f008b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-25 09:15:06 +0530

    code

---
+++ script.js
@@ -0,0 +1,35 @@
+// function toggleTheme() {
+//       document.body.classList.toggle('dark-theme');
+//     }
+
+let body = document.querySelector("body");
+let button = document.querySelector(".nightmode");
+let navbar = document.querySelector(".navbar");
+
+button.addEventListener("click", () => {
+  if (body.style.backgroundColor === "white" || body.style.backgroundColor === "") {
+    body.style.backgroundColor = "#555";
+    body.style.color = "white";
+    navbar.style.backgroundColor = "#555";
+    navbar.style.color = "white";
+  } else {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+    navbar.style.backgroundColor = "white";
+    navbar.style.color = "black";
+  }
+});
+
+var typed = new Typed(".typed-text", {
+  strings: ["A Frontend Developer", "A Backend Developer", "A Android Developer" , "A DSA Enthusiast"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true
+});
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
