272c2ed5ff82c6281c196a25cdfc069ee17cede3
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-26 03:40:52 +0530

    code

--- script.js
+++ script.js
@@ -33,3 +33,13 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
+let icon = document.getElementById("iconmoon");
+icon.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+    icon.src = "images/sun.png";
+  }else{
+    icon.src = "images/moon.png"
+  }
+}
+
